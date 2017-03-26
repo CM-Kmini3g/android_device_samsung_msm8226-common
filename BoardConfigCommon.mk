a27f67d2bce909fc4d7c261efa9974dcd47e09a9
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2017-03-25 21:02:17 -0600

    Enable keymaster v0.3 by default TARGET_HW_KEYMASTER_V03 flag is used to control the keymaster versions based on the requirement. Set TARGET_HW_KEYMASTER_V03 flag to true to enable keymaster v0.3 and false for keymaster v1.0.
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -27,6 +27,9 @@ TARGET_CPU_VARIANT := krait
 AUDIO_FEATURE_ENABLED_LOW_LATENCY_CAPTURE := true
 BOARD_USES_ALSA_AUDIO := true
 
+# Control flag between KM versions
+TARGET_HW_KEYMASTER_V03 := true
+
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_QCOM := true
@@ -75,7 +78,7 @@ TARGET_QCOM_NO_FM_FIRMWARE := true
 EXTENDED_FONT_FOOTPRINT := true
 
 # Init
-TARGET_INIT_VENDOR_LIB := libinit_msm
+#TARGET_INIT_VENDOR_LIB := libinit_msm
 
 # Memory
 MALLOC_IMPL := dlmalloc
