675945e12fc22580760b279df2bf2f29aea50acd
kn06497 <kn06497@hotmail.com>
2017-04-15 14:30:04 -0500

    msm8226: Disable FM radio Kmini3g no use it
    Signed-off-by: kn06497 <kn06497@hotmail.com>

--- msm8226.mk
+++ msm8226.mk
@@ -68,11 +68,6 @@ PRODUCT_PACKAGES += \
     ethertypes \
     libebtc
 
-# FM
-PRODUCT_PACKAGES += \
-    FMRadio \
-    libfmjni
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
     $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
